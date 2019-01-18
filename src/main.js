b7132442cb76facd3aa578538c6213cb904fc0e4
YourOwen <minyuwen@126.com>
2019-01-18 20:35:04 +0800

    配置插件

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 //引入element-ui
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import App from './App'
+Vue.use(ElementUI);
+
 import router from './router'
+
+//引入vuex
+import Vuex from 'vuex'
+Vue.use(Vuex);
+
+
+import App from './App'
 
 
 
